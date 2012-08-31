6ffe2c3b74d33f3ba8d5a4419ca24e25b6bdff7c
silver3x <xsecretx2002@yahoo.com>
2012-08-31 17:26:21 +0700

    update

--- app/helpers/post_helper.rb
+++ app/helpers/post_helper.rb
@@ -2,7 +2,7 @@ module PostHelper
 	def post_error
 		if !flash[:post_warning].nil? 
 			html = '<div class="alert alert-error fade in">'
-		    html += '	<button type="button" class="close" data-dismiss="alert">×</button>'
+		    html += '	<button type="button" class="close" data-dismiss="alert">x</button>'
 		    html += '	<ul>'
 				flash[:post_warning].each do |e| 
 		    		html += "	<li>#{e[1]}</li> "
