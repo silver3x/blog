e5605167d45fb9169ef3e3c93f0faa61297bb1b8
silver3x <xsecretx2002@yahoo.com>
2012-08-31 17:03:38 +0700

    update

--- db/migrate/20120828084745_create_database.rb
+++ db/migrate/20120828084745_create_database.rb
@@ -1,32 +1,29 @@
 class CreateDatabase < ActiveRecord::Migration
   def self.up
+    create_table "user", :force => true do |t|
+        t.string "username", :limit => 50
+        t.string "password", :limit => 50
+        t.string "avatar"
+      end
 
-  create_table "user", :force => true do |t|
-      t.string "username", :limit => 50
-      t.string "password", :limit => 50
-      t.string "avatar"
+    create_table "post", :force => true do |t|
+      t.integer  "user_id"
+      t.integer  "poster_id"
+      t.text     "content"
+      t.datetime "created_at"
+      t.datetime "updated_at"
     end
 
-  create_table "post", :force => true do |t|
-    t.integer  "user_id"
-    t.integer  "poster_id"
-    t.text     "content"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    	create_table "comment", :force => true do |t|
+      t.integer  "user_id"
+      t.integer  "post_id"
+      t.text     "content"
+      t.datetime "created_at"
+      t.datetime "updated_at"
+    end
     add_foreign_key(:post, :user_id, :user)
-  end
-
-  	create_table "comment", :force => true do |t|
-    t.integer  "user_id"
-    t.integer  "post_id"
-    t.text     "content"
-    t.datetime "created_at"
-    t.datetime "updated_at"
     add_foreign_key(:comment, :user_id, :user)
     add_foreign_key(:comment, :post_id, :post)
-  end
-
-
   end
 
   def self.down
